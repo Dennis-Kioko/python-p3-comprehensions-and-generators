514ded6624efd85a3318cd1d6a591f0ff40102b0
denniskioko <kiokodennis6990@gmail.com>
2024-02-28 17:07:59 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    return even_numbers
+numbers = ([0, 1, 3, 5, 7, 8, 9])
+# print (return_evens(range (10)))
+
 
 def make_exclamation(sentence_list):
-    pass
+    make_exclamation_end = [sentence + "!" for sentence in sentence_list]
+    return make_exclamation_end
+
+sentences =(["Hello", "I'm doing great", "Python is fun"])
+
+# print(make_exclamation(sentences))
